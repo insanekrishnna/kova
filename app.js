9b2fe61c0d051ade3f5f2877fa02da86fd2718fe
Pratham Yadav <iiprathamyadav@gmail.com>
2025-11-04 22:25:16 +0530

    Added Review section

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js"); 
 const ExpressError = require("./utils/ExpressError.js"); 
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 
@@ -110,6 +111,21 @@ app.delete("/listings/:id" , wrapAsync (async ( req , res ) =>{
      res.redirect("/listings");
     }));
 
+    //reviews
+
+    //post route
+    app.post("/listings/:id/reviews", async (req, res) => {
+        let listing = await Listing.findById(req.params.id);
+        let newReview = new Review(req.body.review);
+
+        listing.reviews.push(newReview);
+
+       await newReview.save();
+
+       await listing.save();
+       console.log("New Review Added:", newReview);
+       res.redirect(`/listings/${listing._id}`);
+    })
 
 
 // app.get("/testlisting" , async ( req , res ) =>{
